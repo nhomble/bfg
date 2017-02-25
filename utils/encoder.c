0197830505bd3ecab29c912c21d693fb94a00cbd
nhomble <nhomble@terpmail.umd.edu>
2017-02-25 05:15:50 +0000

    simple bf(message) encoder to quickly write seemingly cryptic messages

---
+++ utils/encoder.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void usage(char *prog){
+	fprintf(stderr, "usage: %s [message to encode]\n", prog);
+}
+
+void encode_char(char c){
+	int i;
+	for(i = 0; i < (int) c; i++)
+		printf("+");
+}
+
+void encode_string(char *message){
+	int length, i;
+	char c;
+	// verbose bf file until I optimize TODO
+	length = strlen(message);
+	for(i = 0; i < length; i++){
+		c = message[i];
+		encode_char(c);
+		printf(".>");
+	}
+	encode_char('\n');
+	encode_char('\r');
+}
+
+int main(int argc, char**argv){
+	char *message;
+	if(argc != 2){
+		usage(argv[0]);
+		return 0;
+	}
+	message = argv[1];
+	encode_string(message);
+	return 0;
+}
